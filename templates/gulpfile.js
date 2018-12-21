eeb7d725d01704207b4a79f180d892b3a4f8a20b
xiemingzhang <xiemingzhang@huivo.com>
2018-12-21 18:20:09 +0800

    no message

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -3,22 +3,22 @@ const fs = require('fs')
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const clean = require('gulp-clean');
+const concat = require('gulp-concat');
 
-gulp.task('default', ['clean', 'copy', 'bable2es5', 'jscompress'], () => {
+gulp.task('default', ['jscompress'], () => {
     console.log("完成")
 });
+// gulp.task('clean', function() {
+//     return gulp.src(['build', 'publish'])
+//         .pipe(clean());
+// })
 
-gulp.task('clean', function() {
-    return gulp.src(['build', 'publish'])
-        .pipe(clean());
-})
+// gulp.task('copy', ['clean'], () => {
+//     return gulp.src('proj/**/*')
+//         .pipe(gulp.dest('build'))
+// });
 
-gulp.task('copy', ['clean'], () => {
-    return gulp.src('proj/**/*')
-        .pipe(gulp.dest('build'))
-});
-
-gulp.task('bable2es5', ['copy'], () => {
+gulp.task('bable2es5', () => {
     return gulp.src(['build/**/src/*.js', 'build/**/src/common/*.js'])
         .pipe(babel({
             presets: ['es2015'],
@@ -28,39 +28,18 @@ gulp.task('bable2es5', ['copy'], () => {
 });
 
 gulp.task('jscompress', ['bable2es5'], () => {
-    return gulp.src(['build/**/src/*.js', 'build/**/src/common/*.js'])
+    return gulp.src(['myRelease/**/src/*.js', 'myRelease/**/src/common/*.js'])
         .pipe(uglify())
         .pipe(gulp.dest(function(file) { return file.base; }));
 })
 
-gulp.task('copy2', ['jscompress'], () => {
-    var dir = fs.readdirSync('build/')
-    dir.map(function(dir) {
-        gulp.src('build/' + dir + '/**')
-            .pipe(gulp.dest('publish'))
-    })
-});
-
-gulp.task('publish', ['copy2'], () => {
-    return gulp.src(['build/**/src/*.js', 'build/**/src/common/*.js'])
-        .pipe(uglify())
-        .pipe(gulp.dest(function(file) { return file.base; }));
-})
+// ///////////////////////
 
 gulp.task('clean3', () => {
     return gulp.src(['myRelease'])
         .pipe(clean());
 });
 
-// gulp.task('copy3', ['clean3'], () => {
-//     var dir = fs.readdirSync('proj/')
-//     dir.map(function(dir) {
-//         if(dir !== '.git'){
-//            gulp.src('proj/' + dir + '/**')
-//             .pipe(gulp.dest('my')) 
-//         }  
-//     })
-// });
 gulp.task('copy3', ['clean3'], () => {
     var dir = fs.readdirSync('games/')
     dir.map(function(dir) {
@@ -71,15 +50,6 @@ gulp.task('copy3', ['clean3'], () => {
     })
 });
 
-// gulp.task('getMy', ['copy3'], () => {//bable2es5
-//     return gulp.src(['my/**/src/*.js', 'my/**/src/common/*.js'])
-//         .pipe(babel({
-//             presets: ['es2015'],
-//             plugins: ["babel-plugin-transform-remove-strict-mode"]
-//         }))
-//         .pipe(gulp.dest(function(file) { return file.base; }));
-// });
-// 
 gulp.task('getMy', ['copy3'], () => {//bable2es5
     return gulp.src(['myRelease/**/src/*.js', 'myRelease/**/src/common/*.js'])
         .pipe(babel({
@@ -90,10 +60,16 @@ gulp.task('getMy', ['copy3'], () => {//bable2es5
 });
 
 // gulp.task('bable2es55', () => {
-//     return gulp.src(['proj/index.js'])
+//     return gulp.src(['myRelease/*.js'])
 //         .pipe(babel({
 //             presets: ['es2015'],
 //             plugins: ["babel-plugin-transform-remove-strict-mode"]
 //         }))
 //         .pipe(gulp.dest(function(file) { return file.base; }));
 // });
+// 
+// gulp.task('ggg', () => {
+//     return gulp.src(['myRelease/*.js'])
+//         .pipe(uglify())
+//         .pipe(gulp.dest(function(file) { return file.base; }));
+// })
