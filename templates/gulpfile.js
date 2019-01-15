e22bfa531580977e84786f7a81298992c91a89a2
xiemingzhang <xiemingzhang@huivo.com>
2019-01-15 10:10:13 +0800

    添加压缩cocosjs的功能

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,25 +1,17 @@
 const gulp = require('gulp');
-const fs = require('fs')
-const uglify = require('gulp-uglify');
+const fs = require('fs');
 const babel = require('gulp-babel');
 const clean = require('gulp-clean');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const sourceMap = require('gulp-sourcemaps');
 
 gulp.task('default', ['jscompress'], () => {
     console.log("完成")
 });
-// gulp.task('clean', function() {
-//     return gulp.src(['build', 'publish'])
-//         .pipe(clean());
-// })
-
-// gulp.task('copy', ['clean'], () => {
-//     return gulp.src('proj/**/*')
-//         .pipe(gulp.dest('build'))
-// });
 
 gulp.task('bable2es5', () => {
-    return gulp.src(['build/**/src/*.js', 'build/**/src/common/*.js'])
+    return gulp.src(['myRelease/**/src/*.js', 'myRelease/**/src/common/*.js'])
         .pipe(babel({
             presets: ['es2015'],
             plugins: ["babel-plugin-transform-remove-strict-mode"]
@@ -29,7 +21,11 @@ gulp.task('bable2es5', () => {
 
 gulp.task('jscompress', ['bable2es5'], () => {
     return gulp.src(['myRelease/**/src/*.js', 'myRelease/**/src/common/*.js'])
-        .pipe(uglify())
+        .pipe(uglify({
+            mangle: true,//类型：Boolean 默认：true 是否修改变量名
+            compress: true,//类型：Boolean 默认：true 是否完全压缩
+            preserveComments: "license" //保留所有注释
+        }))
         .pipe(gulp.dest(function(file) { return file.base; }));
 })
 
@@ -59,17 +55,34 @@ gulp.task('getMy', ['copy3'], () => {//bable2es5
         .pipe(gulp.dest(function(file) { return file.base; }));
 });
 
-// gulp.task('bable2es55', () => {
-//     return gulp.src(['myRelease/*.js'])
-//         .pipe(babel({
-//             presets: ['es2015'],
-//             plugins: ["babel-plugin-transform-remove-strict-mode"]
-//         }))
-//         .pipe(gulp.dest(function(file) { return file.base; }));
-// });
-// 
-// gulp.task('ggg', () => {
-//     return gulp.src(['myRelease/*.js'])
-//         .pipe(uglify())
-//         .pipe(gulp.dest(function(file) { return file.base; }));
-// })
+////////////////////////////
+
+gulp.task('cocosclean', () => {
+    return gulp.src(['cocos', 'maps'])
+        .pipe(clean());
+});
+
+gulp.task('cocosminjs', ['cocosclean'], () => {
+    var pathArr = JSON.parse(fs.readFileSync("./path.json")).pathArr;
+    pathArr = pathArr.map(function(item){
+        return 'myRelease/' + item
+    })
+    return gulp.src(pathArr)
+        .pipe(sourceMap.init())
+        .pipe(concat('mycocos.min.js')) 
+        .pipe(uglify({output:{ 
+            // comments:/.license./ 
+        }}))
+        .pipe( sourceMap.write('maps/',{addComment: false}) )
+        .pipe(gulp.dest('cocos/'));
+})
+
+gulp.task('cocosjs', ['cocosclean'], () => {
+    var pathArr = JSON.parse(fs.readFileSync("./path.json")).pathArr;
+    pathArr = pathArr.map(function(item){
+        return 'myRelease/' + item
+    })
+    return gulp.src(pathArr)
+        .pipe(concat('mycocos.js')) 
+        .pipe(gulp.dest('cocos/'));
+})
