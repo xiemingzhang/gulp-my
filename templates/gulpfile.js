c3525ea6e0599c6b1600bfe5abf3d75ae7b59b57
xiemingzhang <xiemingzhang@huivo.com>
2019-04-11 10:39:36 +0800

    no message

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -5,6 +5,10 @@ const clean = require('gulp-clean');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const sourceMap = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');           //压缩图片1
+const tinypng = require('gulp-tinypng-compress');     //压缩图片2 需要有KEY,获取KEY值https://tinypng.com/dashboard/api
+const tinypng_nokey = require('gulp-tinypng-nokey');    //压缩图片3 免费
+const runSequence = require('run-sequence');
 
 gulp.task('default', ['jscompress'], () => {
     console.log("完成")
@@ -37,13 +41,20 @@ gulp.task('clean3', () => {
 });
 
 gulp.task('copy3', ['clean3'], () => {
-    var dir = fs.readdirSync('games/')
-    dir.map(function(dir) {
+    var pathArr = fs.readdirSync('games/')
+    // pathArr.map(function(dir) {
+    //     if(dir !== '.git'){
+    //        gulp.src('games/' + dir + '/**')
+    //         .pipe(gulp.dest('myRelease')) 
+    //     }  
+    // })
+    pathArr = pathArr.map(function(item){
         if(dir !== '.git'){
-           gulp.src('games/' + dir + '/**')
-            .pipe(gulp.dest('myRelease')) 
-        }  
+            return 'games/' + dir + '/**'
+        } 
     })
+    return gulp.src(pathArr)
+            .pipe(gulp.dest('myRelease')) 
 });
 
 gulp.task('getMy', ['copy3'], () => {//bable2es5
@@ -86,3 +97,103 @@ gulp.task('cocosjs', ['cocosclean'], () => {
         .pipe(concat('mycocos.js')) 
         .pipe(gulp.dest('cocos/'));
 })
+
+gulp.task('jsc', () => {
+    return gulp.src('myRelease/cocos2d.js')
+        .pipe(concat('cocos2d.js'))
+        .pipe(uglify({output:{ 
+            // comments:/.license./ 
+        }}))
+        .pipe(gulp.dest('cocos/'));
+})
+///////////////////////
+const jsSrc = 'js/*.js'
+const cssSrc = 'css/*.css'
+const htmlSrc = '*.html'
+const imgSrc = 'img/*.{jpg,png,gif,ico}'
+
+// gulp.task('img', function () {
+//   return gulp.src(imgSrc)
+//     .pipe(imagemin())
+//     .pipe(gulp.dest('dist/img'))
+// })
+
+gulp.task('copy', function () {
+  return gulp.src([
+    '**/*.*',
+    '!img/**/*.*',
+    '!js/**/*.*',
+    '!rev/**/*.*',
+    '!css/**/*.*',
+    '!node_modules/**/*.*',
+    '!gulpfile.js',
+    '!package.json'
+  ])
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('html', function() {
+  return gulp.src(htmlSrc)
+    .pipe(minifyHtml())
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('css', function () {
+  return gulp.src(cssSrc)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],      // 浏览器版本
+      cascade: true,                       // 美化属性，默认true
+      add: true,                           // 是否添加前缀，默认true
+      remove: true,                        // 删除过时前缀，默认true
+      flexbox: true                       // 为flexbox属性添加前缀，默认true
+    }))
+    .pipe(minifyCss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(rev())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('dist/rev/css'))
+})
+
+////////////////
+//图片压缩1 (感觉压缩程度不够)
+gulp.task('imagemin', function () {
+    gulp.src(imgSrc)
+        .pipe(imagemin({
+            optimizationLevel: 3, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true,    //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true,     //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true       //类型：Boolean 默认：false 多次优化svg直到完全优化
+        }))
+        .pipe(gulp.dest(function(file) { return file.base; }))
+});
+
+//压缩图片2 (需要有KEY,并且每个月只有500张)
+gulp.task('tinypng', ['imagemin'], function() {
+    gulp.src(imgSrc)
+        .pipe(tinypng({
+            key: 'RbcjdCSH0DqwgCn1RmGq0dt6CNLdNDqh',
+            sigFile: imgSrc + '/.tinypng-sigs',
+            log: true
+        }))
+        .pipe(gulp.dest(function(file) { return file.base; }));
+})
+
+//压缩图片3 (免费 常用)
+gulp.task('imgCompress', ['tinypng'], function() {
+    gulp.src(imgSrc)
+        .pipe(tinypng_nokey ())
+        .pipe(gulp.dest(function(file) { return file.base; }));
+})
+
+// gulp.task('imgCompress', function () {
+//     runSequence(
+//         'compress_img',
+//         'tinypng',
+//         'tp');
+// });
+
+
+
